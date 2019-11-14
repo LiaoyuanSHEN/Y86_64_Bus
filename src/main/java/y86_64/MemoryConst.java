b027e6d67a0b9c60d043c8b49ee609c42cbf4150
root <root@Suit-PC.localdomain>
2019-11-14 23:49:10 +0800

    first draft

---
+++ src/main/java/y86_64/MemoryConst.java
@@ -0,0 +1,7 @@
+package y86_64;
+
+public class MemoryConst {
+
+    public static int CONTROL_PORT = 4612;
+
+}
---
+++ src/main/java/y86_64/MemoryTcpServer.java
@@ -0,0 +1,19 @@
+package y86_64;
+
+public class MemoryTcpServer implements Memory {
+
+    @Override
+    public void init(int i) {
+
+    }
+
+    @Override
+    public long read(long l) {
+        return 0;
+    }
+
+    @Override
+    public void write(long l, long l1) {
+
+    }
+}
